7bb5caa346319c5601b3deafb50bcb55622e7ab3
dad <60132399+dad2vad@users.noreply.github.com>
2020-02-01 04:06:17 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ const Home = () => {
       console.log('An error occurred', e);
       setResponse({
         type: 'error',
-        message: e
+        message: JSON.stringify(e)
       });
     }
   };
