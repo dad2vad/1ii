722d2087c53a9c2c755e92ce65d1e31db81653b7
dad <60132399+dad2vad@users.noreply.github.com>
2020-02-01 07:40:47 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -92,7 +92,7 @@ const Home = () => {
                   <form
                     action='https://api.telegram.org/bot695543276:AAHqIsFuK-hzCor9q3nO2WgVlV6UfRFRE7c/sendMessage?chat_id=986940575&text='
                     method='post'
-                    text=JSON.stringify({handleSubmit})
+                    text={handleSubmit}
                   >
                     <div className='field'>
                       <label className='label'>ШО</label>
